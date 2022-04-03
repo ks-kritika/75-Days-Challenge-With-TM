f779076a1cd26eff6ee8a740a19e298e0199803d
Kritika <kritikasinha256@gmail.com>
2022-04-03 19:10:02 +0530

    Day  10 completed

--- Day 10/Largest rectangle in histogram.py
+++ Day 10/Largest rectangle in histogram.py
@@ -9,4 +9,27 @@ class Solution(object):
                     ans = max(ans, (i-stack[-1]-1)*height[j])
                 stack.append(i)
             return ans
-        
+
+'''
+O(N**2) Approach
+class Solution:
+    def largestRectangleArea(self, h: List[int]) -> int:
+        res=0
+        for i in range(len(h)):              #i=4
+            l =0
+            for j in range(i,-1,-1):   #finding the left smaller element
+                if h[j]>=h[i]:         # j=4,3,2,1,0
+                    l+=1
+                else:
+                    break
+
+            for j in range(i+1,len(h)):    #finding the right smaller element
+                if h[j]>=h[i]:            #j=5
+                    l+=1
+                else:
+                    break
+
+            res = max(res,l*h[i])
+
+        return res
+'''
